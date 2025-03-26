c9195c3bd076b4e72a988d2bb123b0604ec2b231
COZYTECH <109769074+COZYTECH@users.noreply.github.com>
2025-03-26 12:23:17 +0100

    products routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,4 +1,6 @@
+
 import express from 'express'
+import productsRoutes from './routes/products/index'
 
 const app = express()
 const port = 3000
@@ -6,6 +8,10 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+
+app.use('/products' , productsRoutes);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
